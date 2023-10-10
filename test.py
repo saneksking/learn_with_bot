4c339aba79fdf5c983f7b32058cae63edfdee10c
Saneksking <saneksking@gmail.com>
2023-10-10 21:57:42 +0700

    Сделан метод в классе CreatePass который создаёт пароли, также сделан метод который создаёт секретную фразу к этому паролю

--- test.py
+++ test.py
@@ -1,45 +1,50 @@
 import random
-import string
+from smartrandom.random_master import RandomMaster
 
 
 """Saneks's pass gen - приложжение для генерации сложных паролей"""
 
 
 class CreatePass:
-    letters = string.ascii_letters
-
     def __init__(self, password_len):
         self.password_len = password_len
 
-    def create_pass(self, cls):
-        return ''.join((random.choice(cls.letters) for _ in range(self.password_len)))
+    def create_password(self):
+        chars = ''.join(random.choice('\n!@#$%^&*()_+{}:"><,.;=-"№;:?') for _ in range(self.password_len))
+        letters = RandomMaster.string.get(length=self.password_len)
+        numbers = str(RandomMaster.number.get(length=self.password_len))
+        return ''.join(random.choice(chars + letters + numbers) for _ in range(self.password_len))
+
+    @staticmethod
+    def create_secret_word():
+        return input('Введите секретную фразу: ')
+
+    @staticmethod
+    def save_password():
+        pass
 
 
 print("Saneks's pass gen - приложжение для генерации сложных паролей")
 print('*' * 80)
+print('1 - Получить пароль; 2 - Сгенерировать пароль; 3 - Выйти')
+choice = int(input('Выберете то, что вы хотите сделать: '))
 while 1:
-    try:
-        def menu():
-            print('1 - Получить пароль; 2 - Сгенерировать пароль; 3 - Выйти')
-            choice = int(input('Выберете то, что вы хотите сделать: '))
-            if choice == 1:
-                pass
-            elif choice == 2:
-                return choice
-            else:
-                pass
-
-
-        if menu() == 1:
+    def menu():
+        if choice == 1:
             pass
-        elif menu() == 2:
-            pass_len = input('Введите длину пароля: ')
-            create = CreatePass(pass_len)
-            print(create.create_pass())
+        elif choice == 2:
+            return choice
+
+    if menu() == 1:
+        pass
+    elif menu() == 2:
+        pass_len = int(input('Введите длину пароля: '))
+        create = CreatePass(password_len=pass_len)
+        secret_word = CreatePass.create_secret_word()
+        print(f'Ваш пароль: {create.create_password()}')
+        break
+    else:
+        break
 
 
-        elif menu() == 3:
-            pass
 
-    except ValueError:
-        continue
--- testtwo.py
+++ testtwo.py
@@ -1,13 +1,13 @@
 import random
 import string
 
-for i in str(range(10)):
-    x = ''.join(random.choice('\n!@#$%^&*()_+{}:"><,.;=-"№;:?'))
-    print(x)
+x = ''.join(random.choice('\n!@#$%^&*()_+{}:"><,.;=-"№;:?') for _ in range(1, 10))
+print(x)
 
 
 class RandomStringMaster:
     letters = string.ascii_letters
+
 
     @classmethod
     def get(cls, length=10):
